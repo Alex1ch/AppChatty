1d453f305f76e3e8078cb5641a7b8c78d801aea5
Alex1ch <lexgorodecky1@gmail.com>
2018-10-19 20:24:11 +0300

    Read/Write funcs + Auth form

--- Client/AppChatty.go
+++ Client/AppChatty.go
@@ -1,55 +1,56 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 
 	"github.com/gotk3/gotk3/gtk"
 )
 
-var connection net.Conn
+var (
+	connection net.Conn
+	buffersize int
 
-var mainWindow gtk.Window
+	mainWindow gtk.Window
+	builder    *gtk.Builder
 
-var settings map[string]string
+	settings map[string]string
+)
 
 func main() {
+	settings = make(map[string]string)
+
 	if parseSettings() != 0 {
 		return
 	}
+
+	if initGtk() != 0 {
+		return
+	}
+
 	if drawMain() != 0 {
 		return
 	}
+
+	connectToServer()
 	gtk.Main()
 
 }
 
 func drawMain() int {
-	//Init
-	gtk.Init(nil)
-	fmt.Println("GTK initialized")
-
-	//Builder init
-	b, err := gtk.BuilderNew()
-	if err != nil {
-		log.Fatal("Error:", err)
-		return 1
-	}
-	err = b.AddFromFile("Layout/Layout.glade")
-	if err != nil {
-		log.Fatal("Error:", err)
-		return 1
-	}
-	fmt.Println("Layout was loaded")
 
 	//Getting objects and defining events
 	//Main window
-	obj, err := b.GetObject("Main_window")
+	obj, err := builder.GetObject("Main_window")
 	if err != nil {
-		log.Fatal("Error:", err)
-		return 2
+		log.Fatal("Error in object getting:", err)
+		return 1
 	}
 	mainWindow := obj.(*gtk.Window)
 	mainWindow.Connect("destroy", func() {
@@ -57,10 +58,10 @@ func drawMain() int {
 	})
 
 	//Send button
-	obj, err = b.GetObject("CloseEvt")
+	obj, err = builder.GetObject("CloseEvt")
 	if err != nil {
 		log.Fatal("Error:", err)
-		return 3
+		return 2
 	}
 	closeBtn := obj.(*gtk.EventBox)
 	closeBtn.Connect("button-release-event", func() {
@@ -68,10 +69,10 @@ func drawMain() int {
 	})
 
 	//Sticker button
-	obj, err = b.GetObject("StickersEvt")
+	obj, err = builder.GetObject("StickersEvt")
 	if err != nil {
 		log.Fatal("Error:", err)
-		return 3
+		return 2
 	}
 	stickersBtn := obj.(*gtk.EventBox)
 	stickersBtn.Connect("button-release-event", func() {
@@ -82,17 +83,187 @@ func drawMain() int {
 	return 0
 }
 
-func connectToServer() {
+func initGtk() int {
+	//Init
+	gtk.Init(nil)
+	fmt.Println("GTK initialized")
 
+	//Builder init
+	var err error
+	builder, err = gtk.BuilderNew()
+	if err != nil {
+		log.Fatal("Error:", err)
+		return 1
+	}
+	err = builder.AddFromFile("Layout/Layout.glade")
+	if err != nil {
+		log.Fatal("Error:", err)
+		return 1
+	}
+	fmt.Println("Layout was loaded")
+	return 0
+}
+
+func connectToServer() int { //connection net.Conn {
+	_, ok := settings["ip"]
+	if ok {
+		//Connecting to server
+		var err error
+		connection, err = net.Dial("tcp", settings["ip"]+":"+settings["port"])
+		if err != nil {
+			popupError("Can't connect to the server\nException: "+err.Error(), "Error")
+			return 1
+		}
+
+		//Showing the auth window
+		obj, err := builder.GetObject("Auth")
+		if err != nil {
+			log.Fatal("Error in object getting:", err)
+			return 2
+		}
+		authWin := obj.(*gtk.Window)
+		authWin.SetPosition(gtk.WIN_POS_CENTER_ON_PARENT)
+		authWin.ShowAll()
+
+		obj, err = builder.GetObject("SignIn")
+		if err != nil {
+			log.Fatal("Error:", err)
+			return 3
+		}
+		SignInBtn := obj.(*gtk.Button)
+		SignInBtn.Connect("clicked", func() {
+			sendPacket(connection, 3, []byte{1, 2, 3, 4, 5})
+		})
+
+		obj, err = builder.GetObject("SignIn")
+		if err != nil {
+			log.Fatal("Error:", err)
+			return 3
+		}
+		SignUpBtn := obj.(*gtk.Button)
+		SignUpBtn.Connect("button-release-event", func() {
+			sendPacket(connection, 3, []byte{1, 2, 3, 4, 5})
+		})
+
+	} else {
+		popupError("The server IP is not defined in settings(file)", "Error")
+		return 2
+	}
+	return 0
 }
 
 func parseSettings() int {
+	settings["buffersize"] = "2048"
+	settings["port"] = "1666"
 	b, err := ioutil.ReadFile("settings")
 	if err != nil {
 		log.Fatal("Error in setting parsing: ", err)
 		return 1
 	}
 	str := string(b)
-	//string.s
+	if len(str) == 0 {
+		log.Fatal("Error: empty settings file")
+		return 1
+	}
+	lines := strings.Split(str, "\n")
+	var split []string
+	for i := range lines {
+		split = strings.Split(lines[i], "=")
+		if len(split) < 2 {
+			continue
+		}
+		settings[split[0]] = split[1]
+	}
+
+	buffersize, err = strconv.Atoi(settings["buffersize"])
+	if err != nil {
+		buffersize = 2048
+		log.Println("Warning: wrong value for \"buffersize\" in settings")
+	}
+
+	return 0
+}
+
+func popupError(content, title string) {
+	popup := gtk.MessageDialogNew(nil, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_NONE, content)
+	popup.SetTitle("Error")
+	popup.ShowAll()
+}
+
+func popupInfo(content, title string) {
+	popup := gtk.MessageDialogNew(nil, 0, gtk.MESSAGE_INFO, gtk.BUTTONS_NONE, content)
+	popup.SetTitle("Error")
+	popup.ShowAll()
+}
+
+func readPacket(client net.Conn) (exitCode int, dataLen uint32, opCode uint16, buffer []byte) {
+	//defer readRecover(client, &exitCode)
+	dataLenB := make([]byte, 4)
+	recLen, err := client.Read(dataLenB)
+	if err != nil {
+		fmt.Println("Error in message receiving(len): " + err.Error())
+		client.Close()
+		exitCode = 1
+		return
+	}
+	dataLen = binary.LittleEndian.Uint32(dataLenB)
+	opCodeB := make([]byte, 2)
+	_, err = client.Read(opCodeB)
+	if err != nil {
+		fmt.Println("Error in message receiving(opCode): " + err.Error())
+		client.Close()
+		exitCode = 1
+		return
+	}
+	opCode = binary.LittleEndian.Uint16(opCodeB)
+	buffer = make([]byte, dataLen)
+	//client.SetDeadline(time.Now().Add(time.Duration(30000)))
+	_, err = client.Read(buffer)
+	if err != nil {
+		fmt.Println("Error in message receiving(data): " + err.Error())
+		client.Close()
+		exitCode = 1
+		return
+	}
+	if recLen == 0 {
+		fmt.Println("Recieved empty message")
+		client.Close()
+		exitCode = 2
+		return
+	}
+	exitCode = 0
+	return
+}
+
+//func readRecover(client net.Conn, exitCode *int) {
+//	if r := recover(); r != nil {
+//		fmt.Println("Recovered from ", r)
+//		*exitCode = 1
+//		client.Close()
+//	}
+//}
+
+func sendPacket(client net.Conn, opCode uint16, data []byte) int {
+	var buffer bytes.Buffer
+	opCodeB := make([]byte, 2)
+	lenB := make([]byte, 4)
+	lenght := len(data)
+	binary.LittleEndian.PutUint32(lenB, uint32(lenght))
+	binary.LittleEndian.PutUint16(opCodeB, opCode)
+
+	buffer.Write(lenB)
+	buffer.Write(opCodeB)
+	buffer.Write(data)
+	sendData := make([]byte, lenght+6)
+	_, err := buffer.Read(sendData)
+	if err != nil {
+		fmt.Println("Error in message forming: " + err.Error())
+		return 1
+	}
+	_, err = client.Write(sendData)
+	if err != nil {
+		fmt.Println("Error in message sending: " + err.Error())
+		return 2
+	}
 	return 0
 }
--- Server/AppChattyServer.go
+++ Server/AppChattyServer.go
@@ -5,47 +5,46 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
-	"time"
 )
 
 const (
-	//ADDRESS Address of the Login Server
+	//ADDRESS Address of the Server
 	ADDRESS = "10.1.1.167"
-	//PORT Listen port for the Login Server
-	PORT       = "1203"
-	BUFFERSIZE = 2048
+	//PORT Listen port for the Server
+	PORT = "1237"
+	//BUFFERSIZE Size of the tcp buffer
+	BUFFERSIZE = 1024
 )
 
 var users map[string]net.Conn
 
-func handlePacket(client net.Conn, buffer []byte, totalLen int) {
-	pkgLen := binary.LittleEndian.Uint32(buffer[0:4])
-	opCode := binary.LittleEndian.Uint16(buffer[4:6])
+func handleNextPacket(client net.Conn) {
 	// Check
-	fmt.Println(pkgLen, opCode)
 
-	recLen, buffer := ReadPacket(client)
+	exitCode, recLen, opCode, buffer := readPacket(client)
+	fmt.Println(exitCode, recLen, opCode, buffer)
 	if recLen == 0 {
 		return
 	}
 
-	handlePacket(client, buffer, recLen)
+	handleNextPacket(client)
 }
 
 func handleSession(client net.Conn) {
-	fmt.Println("Login session started for " + client.RemoteAddr().String())
+	fmt.Println("Session started for " + client.RemoteAddr().String())
 
-	recLen, buffer := ReadPacket(client)
-	if recLen == 0 {
+	exitCode, _, opCode, buffer := readPacket(client)
+	if exitCode != 0 {
 		return
 	}
 
 	var offset uint16 = 4
-	opCode := binary.LittleEndian.Uint16(buffer[offset : offset+2])
 	offset += 2
-	if opCode != 4 {
-		SendPacket(client, 401, []byte{})
+	if !(opCode == 4 || opCode == 5) {
+		fmt.Println("Session error, anauthorized")
+		sendPacket(client, 401, []byte{})
 		client.Close()
+		return
 	}
 	nLen := binary.LittleEndian.Uint16(buffer[offset : offset+2])
 	offset += 2
@@ -56,17 +55,18 @@ func handleSession(client net.Conn) {
 	//password := binary.LittleEndian.Uint16(buffer[offset : offset+passLen])
 
 	users[username] = client
+	fmt.Println("Received register from ", username)
 
-	handlePacket(client, buffer, recLen)
+	handleNextPacket(client)
 }
 
 func listenClient(IP string, PORT string) int {
 	socket, error := net.Listen("tcp", fmt.Sprintf("%s:%s", ADDRESS, PORT))
 	if error != nil {
-		fmt.Println("Error while server startup:\n" + error.Error())
+		fmt.Println("Error while server startup: " + error.Error())
 		return 1
 	}
-	fmt.Printf("Chat server started at %s:%s", ADDRESS, PORT)
+	fmt.Printf("Chat server started at %s:%s\n", ADDRESS, PORT)
 	for {
 		connection, error := socket.Accept()
 		if error != nil {
@@ -83,34 +83,74 @@ func main() {
 	listenClient(ADDRESS, PORT)
 }
 
-func ReadPacket(client net.Conn) (lenght int, buffer []byte) {
-	//Optimization?
-	buffer = make([]byte, BUFFERSIZE)
-	client.SetDeadline(time.Now().Add(time.Duration(30000)))
-	recLen, error := client.Read(buffer)
-	if error != nil {
-		fmt.Println("Error in message receiving:\n" + error.Error())
+func readPacket(client net.Conn) (exitCode int, dataLen uint32, opCode uint16, buffer []byte) {
+	//defer readRecover(client, &exitCode)
+	dataLenB := make([]byte, 4)
+	recLen, err := client.Read(dataLenB)
+	if err != nil {
+		fmt.Println("Error in message receiving(len): " + err.Error())
 		client.Close()
-		lenght = 0
+		exitCode = 1
+		return
+	}
+	dataLen = binary.LittleEndian.Uint32(dataLenB)
+	opCodeB := make([]byte, 2)
+	_, err = client.Read(opCodeB)
+	if err != nil {
+		fmt.Println("Error in message receiving(opCode): " + err.Error())
+		client.Close()
+		exitCode = 1
+		return
+	}
+	opCode = binary.LittleEndian.Uint16(opCodeB)
+	buffer = make([]byte, dataLen)
+	//client.SetDeadline(time.Now().Add(time.Duration(30000)))
+	_, err = client.Read(buffer)
+	if err != nil {
+		fmt.Println("Error in message receiving(data): " + err.Error())
+		client.Close()
+		exitCode = 1
 		return
 	}
 	if recLen == 0 {
 		fmt.Println("Recieved empty message")
 		client.Close()
-		lenght = 0
+		exitCode = 2
 		return
 	}
-	return recLen, buffer
+	exitCode = 0
+	return
 }
 
-func SendPacket(client net.Conn, opCode uint16, data []byte) {
+//func readRecover(client net.Conn, exitCode *int) {
+//	if r := recover(); r != nil {
+//		fmt.Println("Recovered from ", r)
+//		*exitCode = 1
+//		client.Close()
+//	}
+//}
+
+func sendPacket(client net.Conn, opCode uint16, data []byte) int {
 	var buffer bytes.Buffer
 	opCodeB := make([]byte, 2)
 	lenB := make([]byte, 4)
-	binary.LittleEndian.PutUint32(lenB, uint32(len(data)))
+	lenght := len(data)
+	binary.LittleEndian.PutUint32(lenB, uint32(lenght))
 	binary.LittleEndian.PutUint16(opCodeB, opCode)
 
 	buffer.Write(lenB)
 	buffer.Write(opCodeB)
 	buffer.Write(data)
+	sendData := make([]byte, lenght+6)
+	_, err := buffer.Read(sendData)
+	if err != nil {
+		fmt.Println("Error in message forming: " + err.Error())
+		return 1
+	}
+	_, err = client.Write(sendData)
+	if err != nil {
+		fmt.Println("Error in message sending: " + err.Error())
+		return 2
+	}
+	return 0
 }
